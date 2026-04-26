220137578ca5d00579a3585ec63b960ec7db196b
snowbird <zbyszkonatkanski@gmail.com>
2026-04-26 14:50:30 +0200

    Add initial application structure with security configuration and user management

---
+++ microservices-world/TrainTripsUserService/src/test/java/com/example/TrainTripsUserService/TrainTripsUserEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrainTripsUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTripsUserEntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices-world/TrainTripsUserServiceJWT/src/test/java/com/example/TrainTripsUserServiceJWT/TrainTripsUserServiceJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrainTripsUserServiceJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTripsUserServiceJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
